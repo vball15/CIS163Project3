e5e9e9c76970abf2b6770d98e3247047eaef9ff5
Eric Flack <flackeri@Eric-Flacks-MacBook.local>
2015-10-21 18:56:17 -0400

    Initial commit

---
+++ src/BankProgram/tryIt.java
@@ -0,0 +1,11 @@
+package BankProgram;
+
+/**
+ * Created by flackeri on 10/21/15.
+ */
+public class tryIt {
+
+    public static void main(String[] args){
+        System.out.println("Hello World");
+    }
+}
