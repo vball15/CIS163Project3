a3a6b9bc9297c58d09df2ca16166504c6ff13606
EricFlack <flacke@mail.gvsu.edu>
2015-10-23 07:06:07 -0400

    Added abstract account class, checking, savings, bank model, and bank GUI skeleton code.

--- src/BankProgram/tryIt.java
+++ src/BankProgram/tryIt.java
@@ -8,5 +8,6 @@ public class tryIt {
     public static void main(String[] args){
         System.out.println("Hello World");
         System.out.println("This is a change.");
+        System.out.println("GitHub");
     }
 }
